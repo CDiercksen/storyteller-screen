c7f1ac28299b22191554d4b1d5d9ce9de555468e
Christopher <christopher.diercksen@gmail.com>
2022-01-10 00:42:38 -0500

    pseudocode to-do list

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -16,12 +16,25 @@ function Dice() {
     }
     
     return (
-        <Container className="p-5 mb-4 bg-light rounded-3">
-            <Button onClick={rollD100}>D100</Button>
-            
-            <Button onClick={rollD20}>D20</Button>
-        </Container>
+        <div>
+            <Container className="p-5 mb-4 bg-light rounded-3">
+                <Button onClick={rollD20}>D20</Button>
+                <Button onClick={rollD100}>D100</Button>
+            </Container>
+        </div>
     )
 }
 
 export default Dice;
+
+// To Do List: 
+//import boostrap grid and stack components and utilize
+// Build Roll Tables Resource in backend
+// Build table Components in frontent: 
+//      include name, roll button, clear button, 
+//      container to hold results
+// connect frontend to backend for tables
+// Build Player Character Resource in Backend
+// Build Player Character List component in frontend
+// Build AddPlayerCharacter form component in frontend
+// connect player character frontend to backend
