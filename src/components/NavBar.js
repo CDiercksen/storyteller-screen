32ce795020715cb3bc854f4b537e5dad222226b1
Christopher <christopher.diercksen@gmail.com>
2022-02-14 12:47:13 -0500

    cleanedup Dom className elements in NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,22 +4,23 @@ import { Navbar } from "react-bootstrap";
 
   function NavBar() {
       return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="/">Home</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="/">Home</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="/dice">Dice</a>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+              <li className="nav-item active">
+                <a className="nav-link" href="/dice">Dice</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/adventurers">Adventurers</a>
+              <li className="nav-item">
+                <a className="nav-link" href="/adventurers">Adventurers</a>
               </li>
              </ul>
           </div>
         </nav>
+          
       )
   }
 
